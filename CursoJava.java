f78c0e257865767999e09fd22e89c953f2a51c90
Nicolas Baraldo <121053856+NicolasBaraldodeLima@users.noreply.github.com>
2023-11-29 17:12:31 -0300

    Add files via upload

---
+++ CursoJava.java
@@ -0,0 +1,38 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class CursoJava {
+
+    public static void main(String[] args) {
+
+        int nota = 65;
+        int faltas = 10;
+        int maxFaltas = 5;
+        int media = 60;
+        String res;
+
+        res = (nota >= media ? "Aprovado" : "Reprovado");
+        System.out.println("Resultado: " + res);
+
+
+
+        /**
+        if ((nota >= media) && (faltas <= maxFaltas )) {
+
+            System.out.println("Aprovado");
+        } else if (nota >= media) {
+            System.out.println("Recuperacao");
+            
+        } else {
+            System.out.println("Reprovado");
+        }
+
+        System.out.println("Fim do programa");
+
+    }
+}
+
+*/
+
+    }
+
+}
